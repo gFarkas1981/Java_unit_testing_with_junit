624db482a5b95ab4566b59bf8a0ee69673cb3aa5
gFarkas1981 <info@gfarkas.com>
2019-09-03 17:44:43 +0200

    First Unit Test - Red bar

---
+++ tests/StringTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class StringTest {
+
+    @Test
+    void test() {
+
+        fail("Not yet implemented");
+
+    }
+
+}
